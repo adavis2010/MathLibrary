ce525cf9d3eb2e8254898fe95bfd5d242bcf0b0f
max-student <max-student@maxtrain.com>
2021-02-16 14:47:25 -0500

    Add project files.

---
+++ MathLibrary/Math.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLibrary {
+    class Math {
+
+        public static int Add(int a, int b) {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b) {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b) {
+            return a * b;
+        }
+
+        public static int Divide(int a, int b) {
+            return a / b;
+        }
+
+        public static int Modulo(int a, int b) {
+            return a - (a / b * b);
+        }
+
+        public static int Power(int a, int b) {
+            var answer = 1;
+            for (var idx = 0; idx < b; idx++) {
+                answer *= a;
+            }
+
+            return answer;
+
+        }
+        public static bool IsEven(int a) {
+            if ((Modulo(a, 2)) == 0) { return true; } else { return false; }
+        }
+
+        public static int Increment(int a) {
+            return a + 1;
+        }
+        public static int Decrement(int a) {
+            return a - 1;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ MathLibrary/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MathLibrary {
+    class Program {
+        static void Main(string[] args) {
+
+            //var math = new Math(); changed to static method
+            var answer = Math.Add(1,2);
+            Console.WriteLine($" 1 plus 2 equals {answer}");
+
+            answer = Math.Subtract(7,2);
+            Console.WriteLine($" 7 minus 2 equals {answer}");
+
+            answer = Math.Multiply(3, 2);
+            Console.WriteLine($" 3 multiply 2 equals {answer}");
+
+            answer = Math.Divide(15, 5);
+            Console.WriteLine($" 15 divided 5 equals {answer}");
+
+            answer = Math.Modulo(13, 5); // should return 3
+            Console.WriteLine($" 13 Modulo 5 equals {answer}");
+
+            answer = Math.Modulo(7, 2); // should return 1
+            Console.WriteLine($" 7 Modulo 2 equals {answer}");
+
+            answer = Math.Modulo(1234, 13); // should return 12
+            Console.WriteLine($" 1234 Modulo 13 equals {answer}");
+
+            var a =2;
+            var b =4;
+            answer = Math.Power(a, b); //should return 16
+            Console.WriteLine($" {a} power {b} equals {answer}");
+
+              a =7;
+              b =4;
+             answer = Math.Power(a, b); //should return 2401
+             Console.WriteLine($" {a} power {b} equals {answer}");
+
+            a = 7;
+            var boolAnswer = Math.IsEven(a);
+            Console.WriteLine($"{a} is {(boolAnswer ? "Even" : "Odd")}");
+
+            a = 44;
+            boolAnswer = Math.IsEven(a);
+            Console.WriteLine($"{a} is {(boolAnswer ? "Even" : "Odd")}");
+
+            Console.WriteLine($"{a} incremented is { Math.Increment(a)}");
+            Console.WriteLine($"{a} decremented is { Math.Decrement(a)}");
+
+
+        }
+
+
+
+    }
+}
